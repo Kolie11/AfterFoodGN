fe9774f8c79d59a4aa87b4b81be6d2634171916e
Kolie11 <151475303+Kolie11@users.noreply.github.com>
2024-06-11 21:32:34 +0200

    siteweb resto

--- app.js
+++ app.js
@@ -13,28 +13,3 @@ allLinks.forEach((item) => {
   });
 });
 
-//-------Slider
-
-// const slider = document.querySelector('.slider');
-// const prevBtn = document.querySelector('.prev-btn');
-// const nextBtn = document.querySelector('.next-btn');
-
-// let currentSlide = 0;
-
-// function showSlide(index) {
-//   const slides = document.querySelectorAll('.img-box');
-//   slides.forEach((slide, i) => {
-//     slide.style.transform = `translateX(${100 * (i - index)}%)`;
-//   });
-// }
-
-// prevBtn.addEventListener('click', () => {
-//   currentSlide = Math.max(currentSlide - 1, 0);
-//   showSlide(currentSlide);
-// });
-
-// nextBtn.addEventListener('click', () => {
-//   const slides = document.querySelectorAll('.img-box');
-//   currentSlide = Math.min(currentSlide + 1, slides.length - 1);
-//   showSlide(currentSlide);
-// });
